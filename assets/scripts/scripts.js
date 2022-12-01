0c6e7d37d2a68ee244400d9e5e418a5d46f9ce52
Ryan da Silva de Castro Pereira <ryandasilva1008@gmail.com>
2022-12-01 19:30:01 -0300

    adicionando as redes sociais

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -6,14 +6,14 @@ let repos = []
 fetch('https://api.github.com/users/Ryan-Castro/repos').then(async(response)=>{
     repos = await response.json()
     repos.forEach(repo => {
-        $("#repositories").children().append(`
+       if(repo.name != "Meu-Portifolio"){ $("#repositories").children().append(`
             <a href="${repo.html_url}" target="_blank" rel="external">
                 <div class="repo">
                     <h1>${repo.name}</h1>
                     <p>${repo.description}</p>
                 </div>
             </a>
-    `)
+    `)}
     });
 
 })
